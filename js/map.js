70334dc23e7ec3670636b0ab030621c8f42b8737
Dillon Rice <drice89@gmail.com>
2020-05-18 20:48:49 -0700

    Add viewport information

--- js/map.js
+++ js/map.js
@@ -25,7 +25,8 @@ function Character() {
   this.dimensions = [30, 30];
   this.position = [51,51]
   //movement speed
-  this.delayMove = 100;
+  this.delayMove = 200;
+  this.facing = "down"
 }
 
 Character.prototype.placeAt = function(x,y) {
@@ -72,8 +73,7 @@ Character.prototype.processMovement = function(time) {
   return true
 }
 
-
-    let player = new Character();
+let player = new Character();
     
     const gameMap =[ 
              1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
@@ -147,17 +147,20 @@ Character.prototype.processMovement = function(time) {
     return ((y*mapW) + x);
   }
 
-  function validator() {
-    x = player.tileFrom[0]
-    y = player.tileFrom[0]
+  function inBounds() {
+    const x = player.tileFrom[0]
+    const y = player.tileFrom[1]
 
     return (x > 0 && x < (mapW-1)) && (y > 0 && y < mapH)
   }
 
 
+
+
   //listeners
   window.onload = function() {
-    ctx = document.getElementById('game').getContext('2d');
+    const game = document.getElementById("game")
+    ctx = game.getContext('2d');
     requestAnimationFrame(drawGame);
     ctx.font = "bold 10pt sans-serif";
 
@@ -171,6 +174,8 @@ Character.prototype.processMovement = function(time) {
         keysDown[e.keyCode] = false;
       }
     });
+    //this is setting the viewport screen area 
+    veiwport.screen = [game.width, game.height]
   };
 
   function drawGame() {
@@ -190,117 +195,71 @@ Character.prototype.processMovement = function(time) {
     //Player movement
     if (!player.processMovement(currentFrameTime)) {
             //////////////////////////////////
-      if (validator())
-            //Left + up arrow
-            if ((keysDown[37] && keysDown[38]) 
-                && (player.tileFrom[0] > 0 && player.tileFrom[1] > 0) 
-                && gameMap[toIndex((player.tileFrom[0] - 1), player.tileFrom[1]-1)] !== 1) {
-                  player.tileTo[0] -= 1
-                  player.tileTo[1] -= 1
-            }
-            //Left + down arrow
-            // else if ((keysDown[37] && keysDown[40]) 
-            //     && player.tileFrom[0] > 0 && player.tileFrom[1] < (mapH-1)
-            //     && gameMap[toIndex((player.tileFrom[0] - 1), player.tileFrom[1] + 1)] !== 1)
-            //      {
-            //       console.log("LEFT + DOWN")
-      
-            //       // player.tileTo[0] -= 1
-            // }
-
-      //if the key is pressed and the player is within bounds and that the Up arrow will not 
-      //put a character into a wall (-1 because we are moveing close to index 0)
-      
-     else if (keysDown[38] 
-          //checking that player current of y is > the edge of the board
-          && validator()
-          //checking that the index of the map at current location x, y-1 != wall
-          && gameMap[toIndex(player.tileFrom[0], (player.tileFrom[1] - 1))] !== 1) {
-            player.tileTo[1] -= 1
-            
-      }
-      //Down arrow
-      else if (keysDown[40] 
-          //checking that player current of y is < the edge of the board
-          && player.tileFrom[1] < (mapH-1) 
-          //checking that the index of the map at current location x, y+1 != wall
-          && gameMap[toIndex(player.tileFrom[0], (player.tileFrom[1] + 1))] !== 1) {
-            player.tileTo[1] += 1
-      }
-      //Left arrow
-      else if (keysDown[37] 
-          //tileFrom x value > the edge of the board
-          && player.tileFrom[0] > 0 
-          //tile from x - 1 (moving left), y is not a wall
-          && gameMap[toIndex((player.tileFrom[0] - 1), player.tileFrom[1])] !== 1) {
+      if (inBounds()) {
+        //Left + up arrow
+        if ((keysDown[37] && keysDown[38]) 
+            && gameMap[toIndex((player.tileFrom[0] - 1), player.tileFrom[1] - 1)] !== 1) {
+              player.tileTo[0] -= 1
+              player.tileTo[1] -= 1
+        }
+        //Left + down arrow
+        else if ((keysDown[37] && keysDown[40]) 
+            && gameMap[toIndex((player.tileFrom[0] - 1), player.tileFrom[1] + 1)] !== 1) {
             player.tileTo[0] -= 1
-      }
-      //right arrow
-      else if (keysDown[39] 
-          //tileFrom x value < the edge of the board
-          && player.tileFrom[0] < (mapW-1) 
-          //tile from x + 1 (moving right), y is not a wall
-          && gameMap[toIndex((player.tileFrom[0] + 1), player.tileFrom[1])] !== 1) {
+            player.tileTo[1] += 1
+            
+        }
+        //right + up arrow
+        else if ((keysDown[39] && keysDown[38]) 
+            && gameMap[toIndex((player.tileFrom[0] + 1), player.tileFrom[1] - 1)] !== 1) {
+              player.tileTo[0] += 1
+              player.tileTo[1] -= 1
+        }
+        //right + down arrow
+        else if ((keysDown[39] && keysDown[40]) 
+            && gameMap[toIndex((player.tileFrom[0] + 1), player.tileFrom[1] + 1)] !== 1) {
             player.tileTo[0] += 1
-      }
+            player.tileTo[1] += 1
+        }
 
-
-
-      //right arrow
-      // if (keysDown[39] 
-      //     //tileFrom x value < the edge of the board
-      //     && player.tileFrom[0] < (mapW-1) 
-      //     //tile from x + 1 (moving right), y is not a wall
-      //     && gameMap[toIndex((player.tileFrom[0] + 1), player.tileFrom[1])] !== 1) {
-      //       player.tileTo[0] += 1
-      // }
+        else if (keysDown[38] 
+              //checking that player current of y is > the edge of the board
+              //checking that the index of the map at current location x, y-1 != wall
+              && gameMap[toIndex(player.tileFrom[0], (player.tileFrom[1] - 1))] !== 1) {
+                player.tileTo[1] -= 1
+                
+          }
+          //Down arrow
+          else if (keysDown[40] 
+              //checking that the index of the map at current location x, y+1 != wall
+              && gameMap[toIndex(player.tileFrom[0], (player.tileFrom[1] + 1))] !== 1) {
+                player.tileTo[1] += 1
+          }
+          //Left arrow
+          else if (keysDown[37] 
+              //tile from x - 1 (moving left), y is not a wall
+              && gameMap[toIndex((player.tileFrom[0] - 1), player.tileFrom[1])] !== 1) {
+                player.tileTo[0] -= 1
+          }
+          //right arrow
+          else if (keysDown[39] 
+              //tileFrom x value < the edge of the board
+              && gameMap[toIndex((player.tileFrom[0] + 1), player.tileFrom[1])] !== 1) {
+                player.tileTo[0] += 1
+          }
+    }
       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
       //check to see if player is moving
       if((player.tileFrom[0] !== player.tileTo[0]) || (player.tileFrom[1] !== player.tileTo[1])){
         player.timeMoved = currentFrameTime
       }
       
     }
+
+    veiwport.update(player.position[0] + (player.dimensions[0]/2), player.position[1] + (player.dimensions[1]/2))
     
+    ctx.fillStyle = "#000000";
+    ctx.fillRect = (0,0, veiwport.screen[0], veiwport.scren[1])
     //draw from map
     for(let row = 0; row < mapH; row++) {
       for(let col = 0; col < mapW; col++) {
@@ -326,3 +285,44 @@ Character.prototype.processMovement = function(time) {
   lastFrameTime = currentFrameTime;
   requestAnimationFrame(drawGame)
 }
+
+//viewport
+
+const veiwport = {
+  //dimensions of canvas
+  screen: [0,0],
+  //top left tile
+  start: [0,0],
+  //bottom right tile
+  endTile: [0,0],
+  //x and y offeset from the beginning of the screen
+  offset: [0,0],
+  update: function(px, py) {
+    //px and py are the center of the visible area to the camera
+    //drawing area with/2 - camera horizontal offeset
+    this.offset[0] = Math.floor((this.screen[0]/2) - px);
+    //dawing area height/2 - camera vertical offser
+    this.offset[1] = Math.floor((this.screen[1]/2) - py);
+
+    //this is the x, y value of the tile at the center
+    const tile = [Math.floor(px/tileW), Math.floor(py/tileH)]
+    console.log("center point", tile)
+    //based on center tile defined by camera. horizontal value 
+    //[first tile X] = [center tile X] - 1 - Round Up(([Canvas width] / 2) / [tile width]); x - 9
+    this.startTile[0] = tile[0] - 1 - Math.ceil((this.screen[0]/2) /tileW);
+    // first tile y = center tile y -  9
+    this.startTile[1] = tile[1] - 1 - Math.ceil((this.screen[1]/2) /tileH);
+    console.log(this.startTile)
+    if(this.startTile[0] < 0) this.startTile[0] = 0;
+    if(this.startTile[1] < 0) this.startTile[1] = 1;
+
+    //x + 9
+    this.endTile[0] = tile[0] + 1 + Math.ceil((this.screen[0]/2) /tileW);
+    // y + 9
+    this.endTile[1] = tile[1] + 1 + Math.ceil((this.screen[0]/2) /tileH);
+    console.log(this.endTile)
+    if(this.endTile[0] >= mapW) this.endTile[0] = mapW - 1;
+    if(this.endTile[1] >= mapH) this.endTile[1] = mapH - 1;
+  }
+}
+
